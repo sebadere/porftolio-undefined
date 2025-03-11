27159bccbf982329ca85ce247266768734a8f644
SebaDere <dere1997@gmail.com>
2025-03-10 23:38:36 -0300

    changes

--- src/components/ProjectsListComponent.tsx
+++ src/components/ProjectsListComponent.tsx
@@ -44,7 +44,7 @@ const CarouselContainer = styled.div`
   justify-content: center; /* Center horizontally */
   gap: 12px;/* Spacing between arrows and image */
   @media (max-width: 900px) {  
-    gap: 1px;
+    gap: 0px;
   }
 `;
 
